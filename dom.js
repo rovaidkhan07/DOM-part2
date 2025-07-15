f21629efb6c0a36802e7e52a846bfd2cc2fcca04
rovaidkhan07 <rovaidkundi@gmail.com>
2025-07-15 09:26:42 -0700

    part2

--- dom.js
+++ dom.js
@@ -1,6 +1,19 @@
 //let heading = document.getElementById("heading"); 
 //console.log(heading);
 
-let heading = document.getElementsByClassName("heading");
-console.dir(heading);  
-console.log(heading);
+// let heading = document.getElementsByClassName("heading");
+// console.dir(heading);  
+// console.log(heading);
+
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + " - Updated Text";
+
+let divs = document.querySelectorAll(".box");
+
+let idx = 1;
+for (let div of divs) {
+    div.innerText = 'new unique Box  ${idx}';
+    idx++;
+}
