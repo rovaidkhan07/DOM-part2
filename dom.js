1a9ec5062164ed80181569e850cfe28df46c75b9
rovaidkhan07 <rovaidkundi@gmail.com>
2025-07-15 08:41:39 -0700

    part2

---
+++ dom.js
@@ -0,0 +1,6 @@
+//let heading = document.getElementById("heading"); 
+//console.log(heading);
+
+let heading = document.getElementsByClassName("heading");
+console.dir(heading);  
+console.log(heading);
